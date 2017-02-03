7057e9c86c9c789274432977472b3ffe19f633b3
Kali Norby <kali@ifttt.com>
2017-02-03 14:27:32 -0800

    Adding some better waiting logic around the launchd action

--- bin/d4m-nfs-start.sh
+++ bin/d4m-nfs-start.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-LIBDIR=$(dirname $0)
+LIBDIR=$(cd "$(dirname "$0")" ; pwd -P)
 
 echo "[`date`][d4m-nfs] Waiting 30 seconds for the Docker VM to become ready"
-sleep 30
+sleep 25
 echo "[`date`][d4m-nfs] Starting VM setup"
 
 # check that screen has not already been setup
 if ! $(screen -ls |grep d4m > /dev/null 2>&1); then
-    echo "[`date`][d4m-nfs] Setup 'screen' to work properly with the D4M tty, while at it name it 'd4m'."
-    screen -AmdS d4m ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty
+
+    until $(screen -ls |grep d4m > /dev/null 2>&1); do
+        echo "[`date`][d4m-nfs] Setup 'screen' to work properly with the D4M tty, while at it name it 'd4m'."
+        screen -AmdS d4m ~/Library/Containers/com.docker.docker/Data/com.docker.driver.amd64-linux/tty
+        sleep 5
+    done
 
     rm -f ${LIBDIR}/d4m-done
-    echo "[`date`][d4m-nfs] Run Moby VM d4m-nfs setup script."
-    screen -S d4m -p 0 -X stuff "${LIBDIR}/d4m-mount-nfs.sh
+
+    until [ -e ${LIBDIR}/d4m-done ]; do
+        echo "[`date`][d4m-nfs] Runing Moby VM d4m-nfs setup script."
+        screen -S d4m -p 0 -X stuff "${LIBDIR}/d4m-mount-nfs.sh
 "
+        sleep 15
+    done
 
 fi
